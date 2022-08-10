3e0edd017b2314a85987e81234fcd49709f91a10
Brice <bricel67@gmail.com>
2022-08-10 11:29:14 +0200

    Add requireAuth on routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,39 @@ import { Route, Routes } from "react-router-dom";
 
 import Container from "./components/Layout/Container/Container.jsx";
 import Home from "./pages/Home/Home.jsx";
+import Login from "./components/User/Login.jsx";
+import Register from "./components/User/Register.jsx";
+import RequireAuth from "./utils/RequireAuth.jsx";
 
 function App() {
   return (
     <div className="h-[100vh] text-center bg-fond">
       <Container>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <RequireAuth withAuth={true}>
+                <Home />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RequireAuth withAuth={false}>
+                <Login />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RequireAuth withAuth={false}>
+                <Register />
+              </RequireAuth>
+            }
+          />
         </Routes>
       </Container>
     </div>
