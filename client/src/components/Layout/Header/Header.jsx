e37300addeb56c42cb095dc807b1530cd316740b
Brice <bricel67@gmail.com>
2022-08-11 15:17:52 +0200

    Ajust header component

--- client/src/components/Layout/Header/Header.jsx
+++ client/src/components/Layout/Header/Header.jsx
@@ -1,32 +1,20 @@
 import React from "react";
-import tw from "tailwind-styled-components";
 import { GlobeAltIcon } from "@heroicons/react/solid";
 
 import ConnectionNav from "../../UiComponents/Navigations/ConnectionNav/ConnectionNav.jsx";
 import { useSelector } from "react-redux";
 import NavContainer from "../../UiComponents/Navigations/NavContainer/NavContainer.jsx";
 
-const Container = tw.header`
-    fixed
-    flex
-    flex-col
-    justify-start
-    w-full
-    h-24
-    bg-primary-300
-    shadow-sm
-`;
-
 const Header = () => {
   const { isLogged, infos } = useSelector((state) => state.user);
   return (
-    <Container>
-      <div className="flex justify-center items-center mx-2.5">
+    <div className="fixed flex flex-col justify-start w-full h-20 bg-primary-300 shadow-sm">
+      <div className="flex justify-center items-center mt-2 mx-2.5">
         <GlobeAltIcon className="h-9 text-white" />
         <p className="text-white font-bold text-2xl">Groupomania</p>
       </div>
       {!isLogged ? <ConnectionNav /> : <NavContainer />}
-    </Container>
+    </div>
   );
 };
 
