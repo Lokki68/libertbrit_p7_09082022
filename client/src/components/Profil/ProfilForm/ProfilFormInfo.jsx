420ae21a3f761219425efca74cd2965fdaaeffcd
Brice <bricel67@gmail.com>
2022-09-03 17:00:17 +0200

    profil form info

--- client/src/components/Profil/ProfilForm/ProfilFormInfo.jsx
+++ client/src/components/Profil/ProfilForm/ProfilFormInfo.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { updateUser } from "../../../api/user.js";
 import { loadUserInfosReducer } from "../../../redux/User/userReducer.js";
 import { toast, ToastContainer } from "react-toastify";
@@ -61,26 +61,32 @@ const ProfilFormInfo = () => {
     <div className="h-[100vh] flex flex-col pt-24 justify-start items-center">
       <ToastContainer />
       <div className="w-full backdrop-blur py-2 shadow-md">
-        <h1 className="text-3xl text-primary-300 font-bold">
+        <h1 className="text-2xl text-primary-300 font-bold">
           Modifier vos Informations personnelles
         </h1>
       </div>
-      <div className="relative flex flex-col w-3/4 h-[70%] rounded-xl backdrop-blur m-auto p-4 shadow-lg shadow-primary-100 md:flex-row">
+      <div className="relative flex flex-col w-3/4 max-w-md h-[70%] rounded-xl backdrop-blur m-auto p-6 shadow-lg shadow-primary-100 ">
         <div>
-          <h2>{infos.username}</h2>
+          <h2 className="text-4xl font-semibold text-primary-300">
+            {infos.username}
+          </h2>
         </div>
-        <form onSubmit={handleSubmit}>
-          <div>
+        <form
+          className="flex flex-col w-full h-full mt-8  items-center justify-between"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex flex-col h-2/3 w-full justify-around ">
             <input
+              className="text-center text-xl"
               name="email"
               id="email"
               placeholder="Votre Adresse mail"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-          </div>
-          <div>
+
             <input
+              className="text-center text-xl"
               name="phoneNumber"
               id="phoneNumber"
               placeholder="Votre numéro de poste"
@@ -88,7 +94,14 @@ const ProfilFormInfo = () => {
               value={phoneNumber}
             />
           </div>
-          <button type="submit">Envoyer</button>
+          <div className="flex justify-around  w-full">
+            <Link to="/profil" className="btn w-28 ">
+              Retour
+            </Link>
+            <button className="btn w-28" type="submit">
+              Envoyer
+            </button>
+          </div>
         </form>
       </div>
     </div>
