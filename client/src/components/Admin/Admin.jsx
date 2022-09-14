3c8ff5dbadfcaf636e60ee99755e7fd64ef63e3f
Brice <bricel67@gmail.com>
2022-09-14 14:37:18 +0200

    admin page

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { ChatIcon, UserIcon } from "@heroicons/react/solid";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  const [gestPost, setGestPost] = useState(false);
+  const [gestProfil, setGestProfil] = useState(true);
+
+  const toggleGestion = () => {
+    setGestProfil(!gestProfil);
+    setGestPost(!gestPost);
+  };
+
+  return (
+    <div className="h-[100vh] w-full flex flex-col pt-24 justify-start items-center ">
+      <div className="w-full backdrop-blur py-2 shadow-md">
+        <h1 className=" relative w-full  text-3xl text-primary-300 font-bold">
+          {gestProfil ? "Gestion des Profils" : "Gestion des Posts"}
+          <button
+            onClick={toggleGestion}
+            className="btn flex justify-center items-center absolute top-[50%] translate-y-[-50%] right-0 p-0 w-6 h-6 mr-2 "
+          >
+            {gestProfil ? (
+              <UserIcon className="h-5 w-5" />
+            ) : (
+              <ChatIcon className="h-5 w-5" />
+            )}
+          </button>
+        </h1>
+      </div>
+      <div>admin section</div>
+    </div>
+  );
 };
 
 export default Admin;
